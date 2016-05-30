23cd5c149a190ba4dcdc3e0d4d7546c85c1a28dc
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-05-30 17:16:08 -0400

    Added Reticle!

--- Assets/Scripts/Managers/RaycastingManager.cs
+++ Assets/Scripts/Managers/RaycastingManager.cs
@@ -7,6 +7,13 @@ public class RaycastingManager : MonoBehaviour
     RaycastHit myHit; //Declaring a hit variable, registers a ray hit (gets information on hit)
     Vector3 startMarker; //Declaring a container for the start position for the ray	
 
+    void Start()
+    {
+        //Locks cursor and makes it invisible
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
     void Update()
     {
         //Assign in the real time start position for the ray
@@ -21,6 +28,12 @@ public class RaycastingManager : MonoBehaviour
         Debug.DrawRay(startMarker, myRay.direction * 1000, Color.magenta);
         //Project a constant ray by getting the directions = Vector 3 Start( so where the ray starts from) , Vector 3 Direction (myRay.direction (direction of they ray) *1000 (multiplies how long the ray is), and Colour
 
+        //Unlocks the cursor *DEV*
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
     }
 
     public GameObject CastRay() // what the ray does (interaction)
@@ -32,7 +45,7 @@ public class RaycastingManager : MonoBehaviour
         //Print collider hit, filter null errors
         if (myHit.collider != null) //if the ray is hits a collider and does not equal to null:
         {
-            //print (myHit.collider.gameObject.GetComponent<C_Tiles>().id);
+            print(myHit.collider.gameObject);
             return myHit.collider.gameObject; // gets the actual game object within the scene, scripts and all
         }
 
